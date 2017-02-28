2ec08d747cb492a3e5cfbfddaa2ac47dfc70ff07
micaimanong <carlos_wl@163.com>
2017-02-28 16:40:05 +0800

    Initial Commit

---
+++ MyStaticLibraryDM/MyStaticLibraryDM.h
@@ -0,0 +1,13 @@
+//
+//  MyStaticLibraryDM.h
+//  MyStaticLibraryDM
+//
+//  Created by Eric on 2017/2/28.
+//  Copyright © 2017年 micaimanong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyStaticLibraryDM : NSObject
+
+@end
---
+++ MyStaticLibraryDM/MyStaticLibraryDM.m
@@ -0,0 +1,13 @@
+//
+//  MyStaticLibraryDM.m
+//  MyStaticLibraryDM
+//
+//  Created by Eric on 2017/2/28.
+//  Copyright © 2017年 micaimanong. All rights reserved.
+//
+
+#import "MyStaticLibraryDM.h"
+
+@implementation MyStaticLibraryDM
+
+@end
